a409f594f433f7aaabd9182f2df6e6960b4915c7
baraah_adnan <baraahadnan@gmail.com>
2024-10-03 14:43:20 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            string name=Console.ReadLine();
+            Console.WriteLine(name);
+            //2
+            double d = 3.4;
+            Console.WriteLine(d);
+            string s = "baraah";
+            Console.WriteLine(s);   
+            char c= 'A';
+            Console.WriteLine(c);
+            bool b = true;
+            Console.WriteLine(b);
+            int i = 0;
+            Console.WriteLine(i);
+            const Con=
+        }
+    }
+}
